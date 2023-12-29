5ceeae96c6c6d30ab57fbf9670fb0f7410ed4fe1
MoonstruckOccultist <justgive1234@gmail.com>
2023-12-28 19:06:32 -0700

    Spent 3 hours making it look pretty despite being JS heavy task lol

---
+++ Assets/script.js
@@ -0,0 +1,69 @@
+var qBody = document.getElementById('quiz');
+var button = document.getElementById('B');
+
+let QsAndAs = [
+    {
+        "Q": "Is javascript hard?",
+        "A": ["Not at all", "kinda", "yes", "FUCKING EXTREMELY"],
+        "correct": 3,
+    },
+    // {
+    //     "Q": "",
+    //     "A": ["", "", "", ""],
+    //     "correct": ,
+    // },
+    // {
+    //     "Q": "",
+    //     "A": ["", "", "", ""],
+    //     "correct": ,
+    // },
+    // {
+    //     "Q": "",
+    //     "A": ["", "", "", ""],
+    //     "correct": ,
+    // },
+    // {
+    //     "Q": "",
+    //     "A": ["", "", "", ""],
+    //     "correct": ,
+    // },
+    // {
+    //     "Q": "",
+    //     "A": ["", "", "", ""],
+    //     "correct": ,
+    // },
+    // {
+    //     "Q": "",
+    //     "A": ["", "", "", ""],
+    //     "correct": ,
+    // },
+    // {
+    //     "Q": "",
+    //     "A": ["", "", "", ""],
+    //     "correct": ,
+    // },
+    // {
+    //     "Q": "",
+    //     "A": ["", "", "", ""],
+    //     "correct": ,
+    // },
+    // {
+    //     "Q": "",
+    //     "A": ["", "", "", ""],
+    //     "correct": ,
+    // }
+]
+
+console.log(QsAndAs)
+console.log(QsAndAs[0])
+console.log(QsAndAs[0].Q)
+
+function startQuiz() {
+
+};
+
+
+
+
+
+button.addEventListener("submit" , startQuiz());
